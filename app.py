2acd8d683dece06eae6b4f70e3ee93a55f74e0d5
GabrielDornelles <kasugayui2@gmail.com>
2023-06-01 11:45:19 -0300

    add CORS funcionality to accept localhost

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import uvicorn
 from dotenv import load_dotenv, find_dotenv
 from fastapi import FastAPI
 from routers.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv(find_dotenv())
 ENVIRONMENT_NAME = os.getenv("ENVIRONMENT_NAME", None)
@@ -17,6 +18,13 @@ else:
         description="",
         version="0.0.1")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost"],  # Set this to restrict the origins if desired
+    allow_credentials=True,
+    allow_methods=["*"],  # Set the allowed HTTP methods
+    allow_headers=["*"],  # Set the allowed headers
+)
 app.include_router(router)
 
 if __name__ == '__main__':
